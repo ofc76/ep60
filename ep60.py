c5613b006dffb3dbfcc19a44db3e6b8849fe4b5d
ofc76 <ofc76@mail.ru>
2019-01-25 14:59:02 +0200

    3th commit

--- ep60.py
+++ ep60.py
@@ -2,24 +2,31 @@
 # -*- coding: utf-8 -*-
 
 import serial, time 
+from ep60_alig import *
 
 DEVICE_PORT = '/dev/ttyEP60'
 DEVICE_SPEED = 57600
+STR_WIDTH = 36
 
 ESC = 0x1b
 BOLD = 0x47
+DOUBLE = 0x21
 CUT = 0x69
 LF = 0x0a      # print end feed
 CR = 0x0d      # print, N.B. This command is ignored.
 STATUS = 0x76
 BEEP = 0x07
+GS_k = [0x1d,0x6b]
+GS_B = [0x1d,0x42]
+CODE_EAN13 = 67
+CODE128 = 73
 
-HEAD0 = 'Название организации'
+HEAD0 = 'Назва автостанції'
 HEAD1 = ''
-HEAD2 = '      адрес'
+HEAD2 = 'адреса автостанції'
 FOOTER0 = '------------------------------------'
-FOOTER1 = '          чек не фискальный  '
-FOOTER2 = '            Вдалої поїздки  '
+FOOTER1 = 'чек не фискальный'
+FOOTER2 = 'Вдалої поїздки'
 
 class Ep60:
         
@@ -42,6 +49,60 @@ class Ep60:
             arr = f.read()
         return arr
 
+    def barcodePDF(self, code):
+        arr = self.utf_to_866bytes(code)
+        str_prn = bytearray()
+        str_prn.extend(GS_k)
+        str_prn.extend([9,0])
+#        str_prn.append(len(code)+2)
+        str_prn.extend([123,65])
+        str_prn.extend(arr)
+        str_prn.append(0x0)
+        for i in str_prn:
+#            print(i)
+            print('hex: {0:02x} - {1}  - {2}'.format(i,i,i.to_bytes(1,byteorder='big')))
+        self.printer.write(str_prn)
+
+    def barcode128(self, code):
+        arr = self.utf_to_866bytes(code)
+        arr128 = []
+        count = 0
+        for i in arr:
+            print(i)
+            x = int(i) - 48
+            print(x)
+            if count % 2 == 0:
+                left = x * 10
+            else:
+                arr128.append(x + left)
+            count += 1
+        str_prn = bytearray()
+        str_prn.extend(GS_k)
+        str_prn.append(CODE128)
+#        str_prn.append(len(code) +2)
+        str_prn.append(len(arr128) + 2)
+        str_prn.extend([123,67])
+        str_prn.extend(bytes(arr128))
+#        str_prn.append(0x0)
+        for i in str_prn:
+#            print(i)
+            print('hex: {0:02x} - {1}  - {2}'.format(i,i,i.to_bytes(1,byteorder='big')))
+        self.printer.write(str_prn)
+
+    def barcodeEAN13(self, code):
+        arr = self.utf_to_866bytes(code)
+        str_prn = bytearray()
+        str_prn.extend(GS_k)
+        str_prn.append(CODE_EAN13)
+        str_prn.append(len(code))
+        str_prn.extend(arr)
+        for i in str_prn:
+#            print(i)
+            print('hex: {0:02x} - {1}'.format(i,i))
+        self.printer.write(str_prn)
+
+
+
     def print_bold(self, txt):
         arr = self.utf_to_866bytes(txt)
         str_prn = bytearray()
@@ -54,13 +115,34 @@ class Ep60:
         str_prn.append(0)
         self.printer.write(str_prn)
 
+    def print_double(self, txt):
+        arr = self.utf_to_866bytes(txt)
+        str_prn = bytearray()
+        str_prn.append(ESC)
+        str_prn.append(DOUBLE)
+        str_prn.append(32)
+        str_prn.extend(arr)
+        str_prn.append(ESC)
+        str_prn.append(DOUBLE)
+        str_prn.append(0)
+        self.printer.write(str_prn)
+
+    def print_BW_invert(self, txt):
+        arr = self.utf_to_866bytes(txt)
+        str_prn = bytearray()
+        str_prn.extend(GS_B)
+        str_prn.append(1)
+        str_prn.extend(arr)
+        str_prn.extend(GS_B)
+        str_prn.append(0)
+        self.printer.write(str_prn)
+
 
     def print_txt(self, txt):
         arr = self.utf_to_866bytes(txt)
         str_prn = bytearray()
         str_prn.extend(arr)
         self.printer.write(str_prn)
-
 
     def cut(self):
         str_prn = bytearray()
@@ -77,7 +159,7 @@ class Ep60:
     def status(self):
         str_prn = bytearray()
         str_prn.append(ESC)
-        str_prn.append(CUT)
+        str_prn.append(0x76)
         self.printer.write(str_prn)
         time.sleep(0.5)
         state = self.printer.read(1) 
@@ -87,19 +169,19 @@ class Ep60:
     def head(self):
         if not HEAD0 == '':
             str_prn = bytearray()
-            arr = self.utf_to_866bytes(HEAD0)
+            arr = self.utf_to_866bytes(a_center(HEAD0))
             str_prn.extend(arr)
             self.printer.write(str_prn)
             self.LF()
         if not HEAD1 == '':
             str_prn = bytearray()
-            arr = self.utf_to_866bytes(HEAD1)
+            arr = self.utf_to_866bytes(a_center(HEAD1))
             str_prn.extend(arr)
             self.printer.write(str_prn)
             self.LF()
         if not HEAD2 == '':
             str_prn = bytearray()
-            arr = self.utf_to_866bytes(HEAD2)
+            arr = self.utf_to_866bytes(a_center(HEAD2))
             str_prn.extend(arr)
             self.printer.write(str_prn)
             self.LF()
@@ -107,19 +189,19 @@ class Ep60:
     def footer(self):
         if not FOOTER0 == '':
             str_prn = bytearray()
-            arr = self.utf_to_866bytes(FOOTER0)
+            arr = self.utf_to_866bytes(a_center(FOOTER0))
             str_prn.extend(arr)
             self.printer.write(str_prn)
             self.LF()
         if not FOOTER1 == '':
             str_prn = bytearray()
-            arr = self.utf_to_866bytes(FOOTER1)
+            arr = self.utf_to_866bytes(a_center(FOOTER1))
             str_prn.extend(arr)
             self.printer.write(str_prn)
             self.LF()
         if not FOOTER2 == '':
             str_prn = bytearray()
-            arr = self.utf_to_866bytes(FOOTER2)
+            arr = self.utf_to_866bytes(a_center(FOOTER2))
             str_prn.extend(arr)
             self.printer.write(str_prn)
             self.LF()
